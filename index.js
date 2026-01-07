914680c787c233064cf6de95e788a86283e5920f
singhshasank <166588692+singhshasank@users.noreply.github.com>
2026-01-07 12:12:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const messageRoutes = require('./routes/messageRoutes');
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 // 🔥 HARD FIX FOR BROWSER PREFLIGHT
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 // CORS (extra safety)
-app.use(cors());
+
 
 // Body parser
 app.use(express.json());
